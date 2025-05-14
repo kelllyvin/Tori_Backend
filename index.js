fc0d98532fe4190f105d92851259cf33d0810a7b
kelllyvin <kelvinmarvin666@gmail.com>
2025-05-14 12:48:41 +0100

    added cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const cors = require(cors)
+const cors = require("cors")
 const PORT = process.env.port || 3000;
 const userRouter = require("./routes/userRouter");
 // middleware
